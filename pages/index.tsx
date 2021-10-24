db421c60b67648f2596e71d90760113ec9970614
Ronalds Palacis <ronalds.palacis@outlook.com>
2021-10-24 18:27:15 +0300

    Heroku commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import type { NextPage } from "next";
-import { StyledHero } from "../components/styled/IndexWelcome.styled.ts";
-import { StyledVideo } from "../components/styled/IndexVideo.styled.ts";
-import { StyledSteps } from "../components/styled/IndexSteps.styled.ts";
+// import { StyledHero } from "../components/styled/IndexWelcome.styled.ts";
+// import { StyledVideo } from "../components/styled/IndexVideo.styled.ts";
+// import { StyledSteps } from "../components/styled/IndexSteps.styled.ts";
 //import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div>
-    <StyledHero></StyledHero>
+  return (
+    <div>
+      {/* <StyledHero></StyledHero>
     <StyledVideo></StyledVideo>
-    <StyledSteps></StyledSteps>
-  </div>;
+    <StyledSteps></StyledSteps> */}
+    </div>
+  );
 };
 
 export default Home;
